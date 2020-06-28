4a50ac7557facb36705b550c362380be816f32c0
Tema Kyklovod <34029370+temakyklovod31@users.noreply.github.com>
2020-06-29 03:45:59 +0600

    Update MacFinder.py

--- MacFinder.py
+++ MacFinder.py
@@ -3,22 +3,34 @@ import requests
 print('Выберете:\n1 Вычислить 1 MAC адрес\n2 Загрузить файл с Mac адресами')
 a = input('Введите цифру действия:')
 
-if a == '1':
-    mac = input('Введите MAC адрес ')
-    r = requests.get(f'https://api.mylnikov.org/geolocation/wifi?bssid={mac}')
-    rg = r.json()
-    lat = rg['data']['lat']
-    lon = rg['data']['lon']
-    print(lat, lon)
-elif a == '2':
-    wr = open('logs.txt', 'a')
-    for mac in open('mac.txt'):
-        mac = mac.replace('\n', '')
+def one():
+    try:
+        mac = input('Введите MAC адрес ')
         r = requests.get(f'https://api.mylnikov.org/geolocation/wifi?bssid={mac}')
         rg = r.json()
         lat = rg['data']['lat']
         lon = rg['data']['lon']
-        wr.write(f'{lat} {lon}\n')
-    wr.close()
+        print(lat, lon)
+    except Exception:
+        print('Error')
+
+def more():
+    try:
+        wr = open('logs.txt', 'a')
+        for mac in open('mac.txt'):
+            mac = mac.replace('\n', '')
+            r = requests.get(f'https://api.mylnikov.org/geolocation/wifi?bssid={mac}')
+            rg = r.json()
+            lat = rg['data']['lat']
+            lon = rg['data']['lon']
+            wr.write(f'{lat} {lon}\n')
+        wr.close()
+    except Exception:
+        print('Error')
+
+if a == '1':
+    one()
+elif a == '2':
+    more()
 else:
     print('Вы ввели неверное значенние')
